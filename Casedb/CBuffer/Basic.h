641b03cab9daa2334dccb69ce27c42716504a702
iesl-inha-lab <ieslinhalab@gmail.com>
2019-11-25 13:32:27 +0900

    more comments

--- Casedb/CBuffer/Basic.h
+++ Casedb/CBuffer/Basic.h
@@ -1,3 +1,7 @@
+// Copyright (c) 2019 The CaseDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
 #ifndef BASIC_H
 #define BASIC_H
 
